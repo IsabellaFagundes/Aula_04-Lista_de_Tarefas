a0b3d9988b53efef160d035bad409eaaa904f58d
Isabella Fagundes Barao <bellabarao@icloud.com>
2021-04-01 20:36:55 -0300

    criando a lista

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Task {
+  name: string;
+  done: boolean;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,5 +13,18 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  public newTaskName = ""; //guarda o nome inserido no input
+
+  public tasks: Task[] = [ //lista de tarefas
+  ]
+
+  public addTask() { //função de adiciona uma tarefa, e o "status" inicia como "não feita", ou sej,a done: false
+    const newTask = {
+      name: this.newTaskName, //pega o nome guardado em newTaskName
+      done: false,
+    }
+    this.tasks.push(newTask); //da um push da lista, acrescentando a nova tarefa
+    this.newTaskName = ""; //a variável newTaskName volta a ser vazia
+  }
 
 }
